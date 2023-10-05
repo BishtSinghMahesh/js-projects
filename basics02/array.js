6ee3572c031eb8eccc435217dc3f10c0a6e02528
BishtSinghMahesh <119776836+BishtSinghMahesh@users.noreply.github.com>
2023-10-05 17:54:55 +0000

    array

---
+++ basics02/array.js
@@ -0,0 +1,18 @@
+// declare an array
+const myArr = [1, 2, 3, 4, 5];
+const stringArr = ["name", "class", "section", "roll no."]
+
+// get element in an array
+console.log(myArr[0])
+
+// array Methods
+/*
+ Array length            Array toString()
+ Array pop()             Array push()
+ Array shift()           Array unshift()
+ Array join()            Array delete()           
+ Array concat()          Array flat()           
+ Array splice()          Array slice()
+
+*/
+
