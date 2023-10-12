f4b66132b81a24544d6b2675b314a097d3d8603c
BishtSinghMahesh <119776836+BishtSinghMahesh@users.noreply.github.com>
2023-10-12 09:07:22 +0000

    object functions

--- basics02/object.js
+++ basics02/object.js
@@ -10,32 +10,48 @@ const mySym = Symbol("key1");
 
 const obj = {
   name: "Mahesh",
+  surname: "Singh",
+  fullname: "Mahesh Singh",
   age: 20,
   location: "Noida",
   Email: "Maheshsb025@gmail.com",
-  
-  // syntax of using symbol inside an object
-  [mySym]: "symbolValue"
-};
+  LoginDays: ["Monday", "Tuesday"],
 
+  // syntax of using symbol inside an object
+  [mySym]: "symbolValue",
+};
 
 // Access value of specific key
 console.log(obj.age);
 
 // Access value with another methods *important
 // objectName["key"]
-const access2 = obj["age"];
+const access = obj["age"];
+const access2 = obj["fullname"];
+console.log(access);
 console.log(access2);
 
 // declare a Symbol and use it in an object
 // Access symbol value inside an object
-obj[mySym]
+obj[mySym];
 console.log(obj[mySym]);
 
-
-
-// Change value in object 
+// Change value in object
 obj.Email = "MahesSingh@gamil.com";
 
 // Freeze objects so they can't be changed
-Object.freeze(obj);
+// "Object.freeze(obj);" **Imp
+
+// Functions inside objects
+
+obj.greeting = function () {
+  console.log("Hello Human");
+};
+
+obj.greetingTwo = function () {
+  console.log(`Using this keyword inside object function, ${this.fullname}`);
+};
+
+console.log(obj.greeting); // output:- [Function (anonymous)]  function reference
+console.log(obj.greeting()); // output:- Hello Human
+console.log(obj.greetingTwo()); // output:- Using this keyword inside object function, Mahesh Singh
