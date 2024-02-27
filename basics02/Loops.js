842b793acf328faa13b257ac4eb313e1d04ccbc9
BishtSinghMahesh <119776836+BishtSinghMahesh@users.noreply.github.com>
2024-02-27 09:55:32 +0000

    changes

--- basics02/Loops.js
+++ basics02/Loops.js
@@ -79,3 +79,97 @@ const myObject = {
 for (const key in myObject) {
   console.log(`${key} games ${myObject[key]}`);
 }
+
+
+// For in loop in array
+
+const languages = ["js", "rb", "cpp", "java"]
+
+for (const key in languages) {
+  console.log(key);
+}
+// this will print key 0, 1, 2, 3
+
+//  to get the values use
+for (const key in languages) {
+  console.log(languages[key])
+}
+
+// it'll print  js rb cpp java
+
+
+
+//  for in in maps
+// naps are not iteratable , so dont write them in loops
+
+
+
+
+
+
+
+
+// ************************** For Each  loop **********************
+
+const coding = ["js", "cpp", "java", "rb"]
+
+coding.forEach( (val) => {
+  console.log(val);
+});
+
+// this is callback fumnction
+// array.forEach((val) => {
+//  console.log(val)
+// });
+
+// Array.forEach(function(val){
+//   console.log(val)
+// })
+//  either use normal function() or => arrow function
+
+
+
+
+//  you can also give reference to a already declared function
+
+function printme(item){
+  console.log(item)
+}
+
+coding.forEach(printme);
+
+
+// forEach can take more parameteres
+//  codinig.forEach((item, index, arr) => {
+//   console.log(item, index, arr):
+// })
+
+coding.forEach((item, index, arr) => {
+     console.log(item, index, arr);
+  })
+
+
+
+  //  forEach loop in array of objects
+
+  const myFiles = [
+    {
+      languageName: "javascript",
+      languageFileName: "js"
+    },
+    {
+        languageName: "java",
+      languageFileName: "java"
+    },
+   {   languageName: "C++",
+      languageFileName: "cpp"
+    }
+  ]
+
+
+  //  access members of object in array of objects
+
+myFiles.forEach((item) => {
+  console.log(item.languageName)
+})
+
